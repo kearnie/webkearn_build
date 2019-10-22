77d3f9e75af2c9cbf68f580a0887d2fee3dedd24
Kearnie Lin <kearnie@Kearnies-MacBook-Pro-2.local>
2019-10-22 04:21:58 -0400

    white ver temp update

---
+++ precache-manifest.c0051ac127c25cc99224015b5d807206.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d52a4568fa3ec9921e10c3275e0f285b",
+    "url": "/static/media/twitter.d52a4568.svg"
+  },
+  {
+    "revision": "4b693c1c500e8dc9a8765317833979e0",
+    "url": "/static/media/linkedin.4b693c1c.svg"
+  },
+  {
+    "revision": "be3356ba10e7f3536af6bfbbff9d34ea",
+    "url": "/static/media/github.be3356ba.svg"
+  },
+  {
+    "revision": "fc97ace6ff39d84d883490d9d53e14cd",
+    "url": "/static/media/behance.fc97ace6.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2dda3cbdc45fd5cd4939",
+    "url": "/static/js/main.2dda3cbd.chunk.js"
+  },
+  {
+    "revision": "0db9573bfa581347a8de",
+    "url": "/static/js/1.0db9573b.chunk.js"
+  },
+  {
+    "revision": "2dda3cbdc45fd5cd4939",
+    "url": "/static/css/main.fdb73f9d.chunk.css"
+  },
+  {
+    "revision": "e203f20805ed7ea50fe33ca46e18c9bb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd8fffb4fb47b1a937a4b6d499bdb8d9.js"
+  "/precache-manifest.c0051ac127c25cc99224015b5d807206.js"
 );
 
 workbox.clientsClaim();
